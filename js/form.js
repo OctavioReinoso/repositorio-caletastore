ee56988b887c4e66cbeb5ad30e76d00d61dbcf30
Octavio Reinoso <oct.reinoso@gmail.com>
2023-06-10 20:25:04 -0300

    cambios en el set time out

--- js/form.js
+++ js/form.js
@@ -9,11 +9,6 @@ const inputTelefono = document.querySelector('#telefono');
 
 btnEnviar.addEventListener('click', botonEnviar);
 
-<<<<<<< HEAD
-=======
-
-
->>>>>>> b59518beb2c74140bd1d3bc30b1ca235541bc515
 function botonEnviar(e){
     e.preventDefault();
 
@@ -23,33 +18,22 @@ function botonEnviar(e){
             Swal.fire(
                 {
                     icon: 'error',
-<<<<<<< HEAD
                     title: 'Datos incorrectos',
-=======
-                    tittle: 'Datos incorrectos',
->>>>>>> b59518beb2c74140bd1d3bc30b1ca235541bc515
                     text: 'Asegurese de llenar todos los campos',
                 });
         }
 
-<<<<<<< HEAD
     }else if (!validarEmail(inputEmail.value)) {
         Swal.fire({
             icon: 'error',
             title: 'Correo electrónico inválido',
             text: 'Ingrese un correo electrónico válido',
         });
-=======
->>>>>>> b59518beb2c74140bd1d3bc30b1ca235541bc515
     }else{
         btnEnviar.onclick = () => {
             Swal.fire(
                 {
-<<<<<<< HEAD
                     title: 'Enviado',
-=======
-                    tittle: 'Enviado',
->>>>>>> b59518beb2c74140bd1d3bc30b1ca235541bc515
                     text: 'En estos días nos estaremos comunicando con usted. Gracias',
                     icon: 'success'
                 });
@@ -57,11 +41,8 @@ function botonEnviar(e){
     }
 }
 
-<<<<<<< HEAD
 function validarEmail(email) {
     const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return regex.test(email);
 }
 
-=======
->>>>>>> b59518beb2c74140bd1d3bc30b1ca235541bc515
